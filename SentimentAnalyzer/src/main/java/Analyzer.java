a6dfd7dc67bcd09bfd3dbd0042fd6d0eb6b6ec87
Arpan Roy <royarpan09@gmail.com>
2020-08-31 09:19:00 +0530

    SentimentAnalyzer: Implementation completed

--- SentimentAnalyzer/src/main/java/Analyzer.java
+++ SentimentAnalyzer/src/main/java/Analyzer.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -7,6 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -74,23 +75,22 @@ public class Analyzer {
     Pattern pattern = Pattern.compile(regex);
 
     if (sentences != null) {
+      sentences.removeAll(Collections.singleton(null));
       for (Sentence sentence : sentences) {
-        if (sentence != null) {
 
-          String[] token = sentence.getText().split("\\s");
+        String[] token = sentence.getText().split("\\s");
 
-          for (String word : token) {
+        for (String word : token) {
 
-            Matcher matcher = pattern.matcher(word);
+          Matcher matcher = pattern.matcher(word);
 
-            if (matcher.find()) {
+          if (matcher.find()) {
 
-              String validWord = matcher.group().trim().toLowerCase();
-              Word newWord = new Word(validWord);
-              newWord.increaseTotal(sentence.getScore());
+            String validWord = matcher.group().trim().toLowerCase();
+            Word newWord = new Word(validWord);
+            newWord.increaseTotal(sentence.getScore());
 
-              words.add(newWord);
-            }
+            words.add(newWord);
           }
         }
       }
@@ -99,46 +99,110 @@ public class Analyzer {
     return words;
   }
 
-  /*
-   * Implement this method in Part 3
+  /**
+   * This method iterates over each Word in the input Set, uses the Word’s calculateScore method to
+   * get the average sentiment score for that Word, and then places the text of the Word (as key)
+   * and calculated score (as value) in a Map.
+   * <p>
+   * If the input Set of Words is null or is empty, the calculateScores method returns an empty
+   * Map.
+   * <p>
+   * If a Word object in the input Set is null, this method ignores it and process the non-null
+   * Words.
+   *
+   * @param words input Set of Words extracted from the dataset.
+   * @return a map of each word (key) and it's corresponding score (value).
    */
   public static Map<String, Double> calculateScores(Set<Word> words) {
+    Map<String, Double> wordScoreMap = new HashMap<>();
 
-    /* IMPLEMENT THIS METHOD! */
+    if (words != null) {
+      words.removeAll(Collections.singleton(null));
+      for (Word word : words) {
+        String text = word.getText();
+        Double score = word.calculateScore();
 
-    return null; // this line is here only so this code will compile if you don't modify it
+        wordScoreMap.put(text, score);
+      }
+    }
 
+    return wordScoreMap;
   }
 
-  /*
-   * Implement this method in Part 4
+  /**
+   * This method uses the Map of word scores to calculate and return the average score of all the
+   * words in the input sentence and thereby the overall sentiment score of the sentence.
+   * <p>
+   * If a word in the sentence does not start with a letter, it is ignored, but if it starts with a
+   * letter and is not present in the Map, the word score is assumed to be 0.
+   * <p>
+   * If the input Map is null or empty, or if the input sentence is null or empty or does not
+   * contain any valid words, this method returns 0.
+   *
+   * @param wordScores map of each word and it's corresponding score from the dataset.
+   * @param sentence input sentence
+   * @return the overall sentiment score of the sentence
    */
   public static double calculateSentenceScore(Map<String, Double> wordScores, String sentence) {
+    String regex = "^[a-zA-Z]+";
+    Pattern pattern = Pattern.compile(regex);
+    double sentenceScore;
+    try {
+      String[] inputWords = sentence.split("\\s");
+      double totalWords = 0.0;
+      double totalWordScore = 0.0;
 
-    /* IMPLEMENT THIS METHOD! */
+      for (String word : inputWords) {
 
-    return 0; // this line is here only so this code will compile if you don't modify it
+        Matcher matcher = pattern.matcher(word);
 
+        if (matcher.find()) {
+          totalWords++;
+          String validWord = matcher.group().trim().toLowerCase();
+
+          if (wordScores.containsKey(validWord)) {
+            totalWordScore += wordScores.get(validWord);
+          }
+        }
+
+      }
+
+      sentenceScore = totalWordScore / totalWords;
+
+    } catch (NullPointerException e) {
+      return 0;
+    }
+
+    return Double.isNaN(sentenceScore) ? 0 : sentenceScore;
   }
 
-  /*
-   * This method is here to help you run your program. Y
-   * You may modify it as needed.
-   */
-  public static void main(String[] args) throws FileNotFoundException {
-    if (args.length == 0) {
-      System.out.println("Please specify the name of the input file");
-      System.exit(0);
-    }
-    String filename = args[0];
-    System.out.print("Please enter a sentence: ");
-    Scanner in = new Scanner(System.in);
-    String sentence = in.nextLine();
-    in.close();
-    List<Sentence> sentences = Analyzer.readFile(filename);
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    final String projectPath = Paths.get(".").normalize().toAbsolutePath().toString();
+    final String DIRECTORY = projectPath + "\\src\\main\\resources\\";
+    List<Sentence> sentences = Analyzer.readFile(DIRECTORY + "\\reviews.txt");
     Set<Word> words = Analyzer.allWords(sentences);
     Map<String, Double> wordScores = Analyzer.calculateScores(words);
+
+    System.out.print("Please enter a sentence: ");
+
+    String sentence = scanner.nextLine();
+    scanner.close();
+
     double score = Analyzer.calculateSentenceScore(wordScores, sentence);
     System.out.println("The sentiment score is " + score);
+    if (score > 1) {
+      System.out.println("Highly Positive!!");
+    } else if (score > 0 && score <= 1) {
+      System.out.println("Somewhat Positive");
+    } else if (score == 0) {
+      System.out.println("Neutral");
+    } else if (score < 0 && score >= -1) {
+      System.out.println("Somewhat Negative");
+    } else {
+      System.out.println("Negative!!");
+    }
   }
 }
