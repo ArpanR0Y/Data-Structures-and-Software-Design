a99290b9dca24d57d945994548204967dfc8ebc3
Arpan Roy <arpanroy.dev@gmail.com>
2020-09-08 07:50:32 +0530

    MovieRatingsProcessor: First Commit - Project created and Unit Test Cases added

---
+++ MovieRatingsProcessor/src/main/java/MovieRatingsParser.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+public class MovieRatingsParser {
+
+	public static TreeMap<String, PriorityQueue<Integer>> parseMovieRatings(List<UserMovieRating> allUsersRatings) {
+		
+		/* IMPLEMENT THIS METHOD! */
+		
+		return null; // this line is here only so this code will compile if you don't modify it
+	}
+
+}
