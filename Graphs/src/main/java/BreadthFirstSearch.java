1c6ea82d65da45ee7a75a2f236835dca71061a11
Arpan Roy <arpanroy.dev@gmail.com>
2020-09-12 17:39:58 +0530

    Graphs: isHamiltonianPath() method implemented

--- Graphs/src/main/java/BreadthFirstSearch.java
+++ Graphs/src/main/java/BreadthFirstSearch.java
@@ -5,52 +5,54 @@ import java.util.Set;
 
 
 public class BreadthFirstSearch {
-	protected Set<Node> marked;
-	protected Graph graph;
 
-	public BreadthFirstSearch(Graph graphToSearch) {
-		marked = new HashSet<Node>();
-		graph = graphToSearch;
-	}
-	
+  protected Set<Node> marked;
+  protected Graph graph;
 
-	public boolean bfs(Node start, String elementToFind) {
-		if (!graph.containsNode(start)) {
-				return false;
-		}
-		if (start.getElement().equals(elementToFind)) {
-			return true;
-		}
-		Queue<Node> toExplore = new LinkedList<Node>();
-		marked.add(start);
-		toExplore.add(start);
-		while (!toExplore.isEmpty()) {
-			Node current = toExplore.remove();
-			for (Node neighbor : graph.getNodeNeighbors(current)) {
-				if (!marked.contains(neighbor)) {
-					if (neighbor.getElement().equals(elementToFind)) {
-						return true;
-					}
-					marked.add(neighbor);
-					toExplore.add(neighbor);
-				}
-			}
-		}
-		return false;
-	}
+  public BreadthFirstSearch(Graph graphToSearch) {
+    marked = new HashSet<Node>();
+    graph = graphToSearch;
+  }
+
+
+  public boolean bfs(Node start, String elementToFind) {
+    if (!graph.containsNode(start)) {
+      return false;
+    }
+    if (start.getElement().equals(elementToFind)) {
+      return true;
+    }
+    Queue<Node> toExplore = new LinkedList<Node>();
+    marked.add(start);
+    toExplore.add(start);
+    while (!toExplore.isEmpty()) {
+      Node current = toExplore.remove();
+      for (Node neighbor : graph.getNodeNeighbors(current)) {
+        if (!marked.contains(neighbor)) {
+          if (neighbor.getElement().equals(elementToFind)) {
+            return true;
+          }
+          marked.add(neighbor);
+          toExplore.add(neighbor);
+        }
+      }
+    }
+    return false;
+  }
 
   /**
    * This method uses BFS to get the minimum distance from one node to the other. Since BFS goes
    * level by level while exploring nodes, we use that property to get the min distance. Each level
    * represents a unit distance, eg root node is at level 0, therefore, the min distance is also 0.
-   *
+   * <p>
    * A transition to the next level occurs when all the nodes of that level have been explored i.e 0
-   * For every transition to the next level, the min distance is incremented by 1.
-   * The number of nodes to be explored in a new level is denoted by the number of new elements added
-   * to the Queue (toExplore) in the previous level.
-   *
+   * For every transition to the next level, the min distance is incremented by 1. The number of
+   * nodes to be explored in a new level is denoted by the number of new elements added to the Queue
+   * (toExplore) in the previous level.
+   * <p>
    * Since, the destination node will be present in the next level with respect to the current level
-   * we are traversing, therefore, when the destination node is found the min distance + 1 is returned.
+   * we are traversing, therefore, when the destination node is found the min distance + 1 is
+   * returned.
    */
   public int getMinDistance(Node start, String elementToFind) {
     if (start.getElement().equals(elementToFind)) {
@@ -76,7 +78,7 @@ public class BreadthFirstSearch {
           nodesToExploreInNextLevel++;
         }
       }
-      if(nodesToExploreInThisLevel == 0) {
+      if (nodesToExploreInThisLevel == 0) {
         minDistance++;
       }
       nodesToExploreInThisLevel = nodesToExploreInNextLevel;
@@ -84,7 +86,22 @@ public class BreadthFirstSearch {
     return -1;
   }
 
-  public Set<String> getNodesWithinDistance (Graph graph, String src, int distance) {
+  /**
+   * This method uses BFS to get the number of Nodes as a Set within the given distance. Since BFS
+   * goes level by level while exploring nodes, we use that property to get the nodes. Each level
+   * represents a unit distance, eg root node is at level 0, therefore, the distance is also 0.
+   * <p>
+   * A transition to the next level occurs when all the nodes of that level have been explored i.e 0
+   * For every transition to the next level, the currentNodeLevel is incremented by 1. The number of
+   * nodes to be explored in a new level is denoted by the number of new elements added to the Queue
+   * (toExplore) in the previous level.
+   * <p>
+   * Since, during the traversal we look at the next neighbors of the current node, we cover the
+   * nodes from the next level. Therefore, in order to cover all the nodes at or within the given
+   * distance we make sure that the currentNodeLevel < distance. Each neighbouring node's element is
+   * added to the Set of nodes within the distance.
+   */
+  public Set<String> getNodesWithinDistance(Graph graph, String src, int distance) {
     Set<String> nodes = new HashSet<>();
     Node start = graph.getNode(src);
     Queue<Node> toExplore = new LinkedList<>();
@@ -105,11 +122,11 @@ public class BreadthFirstSearch {
           nodesToExploreInNextLevel++;
         }
       }
-      if(nodesToExploreInThisLevel == 0) {
+      if (nodesToExploreInThisLevel == 0) {
         currentNodeLevel++;
       }
       nodesToExploreInThisLevel = nodesToExploreInNextLevel;
-  }
+    }
     return nodes;
   }
 }
--- Graphs/src/main/java/GraphUtils.java
+++ Graphs/src/main/java/GraphUtils.java
@@ -3,36 +3,73 @@ import java.util.Set;
 
 public class GraphUtils {
 
-	public static int minDistance(Graph graph, String src, String dest) {
-	  if (isInValid(graph, src, dest)) {
-	    return -1;
+  /**
+   * Given a Graph, this method returns the shortest distance (in terms of number of edges) from the
+   * node labeled src to the node labeled dest. The method returns -1 for any invalid inputs,
+   * including: null values for the Graph, src, or dest; there is no node labeled src or dest in the
+   * graph; there is no path from src to dest.
+   */
+  public static int minDistance(Graph graph, String src, String dest) {
+    if (isInValid(graph, src, dest)) {
+      return -1;
     }
 
-	  BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch(graph);
+    BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch(graph);
 
-		return breadthFirstSearch.getMinDistance(graph.getNode(src), dest);
-	}
+    return breadthFirstSearch.getMinDistance(graph.getNode(src), dest);
+  }
 
-	public static Set<String> nodesWithinDistance(Graph graph, String src, int distance) {
+  /**
+   * Given a Graph, this method returns a Set of the values of all nodes within the specified
+   * distance (in terms of number of edges) of the node labeled src, i.e. for which the minimum
+   * number of edges from src to that node is less than or equal to the specified distance. The
+   * value of the node itself is not in the Set, even if there is an edge from the node to itself.
+   * The method returns null for any invalid inputs, including: null values for the Graph or src;
+   * there is no node labeled src in the graph; distance less than 1. However, if distance is
+   * greater than or equal to 1 and there are no nodes within that distance (meaning: src is the
+   * only node in the graph), the method returns an empty Set.
+   */
+  public static Set<String> nodesWithinDistance(Graph graph, String src, int distance) {
     if (isInValid(graph, src, distance)) {
       return null;
     }
 
     BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch(graph);
 
-		return breadthFirstSearch.getNodesWithinDistance(graph, src, distance);
-	}
+    return breadthFirstSearch.getNodesWithinDistance(graph, src, distance);
+  }
 
 
-	public static boolean isHamiltonianPath(Graph g, List<String> values) {
+  /**
+   * Given a Graph, this method indicates whether the List of node values represents a Hamiltonian
+   * Path. A Hamiltonian Path is a valid path through the graph in which every node in the graph is
+   * visited exactly once, except for the start and end nodes, which are the same, so that it is a
+   * cycle. If the values in the input List represent a Hamiltonian Path, the method returns true,
+   * otherwise the method returns false , e.g. if the path is not a cycle, if some nodes are not
+   * visited, if some nodes are visited more than once, if some values do not have corresponding
+   * nodes in the graph, if the input is not a valid path (i.e., there is a sequence of nodes in the
+   * List that are not connected by an edge), etc. The method also returns false if the input Graph
+   * or List is null.
+   */
+  public static boolean isHamiltonianPath(Graph graph, List<String> values) {
+    if (isInValid(graph, values)) {
+      return false;
+    }
 
-		/* IMPLEMENT THIS METHOD! */
-		
-		return true; // this line is here only so this code will compile if you don't modify it
-	}
+    if (isCyclic(graph, values)) {
+      Set<Node> nodes = graph.getAllNodes();
+      for (Node node : nodes) {
+        if (!values.contains(node.getElement())) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
 
-	private static boolean isInValid(Graph graph, String src, String dest) {
-	  return graph == null || src == null || dest == null ||
+  private static boolean isInValid(Graph graph, String src, String dest) {
+    return graph == null || src == null || dest == null ||
         !graph.containsNode(graph.getNode(src)) ||
         !graph.containsNode(graph.getNode(dest));
   }
@@ -41,5 +78,29 @@ public class GraphUtils {
     return graph == null || src == null || distance < 1 ||
         !graph.containsNode(graph.getNode(src));
   }
-	
+
+  private static boolean isInValid(Graph graph, List<String> values) {
+    if (graph == null || values == null || values.isEmpty()) {
+      return true;
+    }
+    for (String value : values) {
+      if (!graph.containsNode(graph.getNode(value))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private static boolean isCyclic(Graph graph, List<String> values) {
+    for (int i = 0; i < values.size() - 1; i++) {
+      Node node = graph.getNode(values.get(i));
+      Node nextNode = graph.getNode(values.get(i + 1));
+
+      if (!graph.getNodeNeighbors(node).contains(nextNode)) {
+        return false;
+      }
+    }
+    return values.get(0).equals(values.get(values.size() - 1));
+  }
+
 }
